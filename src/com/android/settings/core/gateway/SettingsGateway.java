ec133ada8d499c150271b39fe186cec9974c0ba8
rituj <ritujbeniwal@gmail.com>
2020-07-25 15:16:48 +0530

    Settings: Introduce automated DC Dimming [2/3]
    * Schedule based on sunset/sunrise
    * Set config_deviceDcDimmingSysfsNode to point to the relevant sysfs
    kernel node
    
    [ghostrider-reborn: adapt to 12, remove brightness-based schedule]
    
    Co-authored-by: Andrew Fluck <andrew@aospa.co>
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: I756c62538bc68c104ac49b84e923f11cf0ce35ce

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -104,6 +104,7 @@ import com.android.settings.deviceinfo.batteryinfo.BatteryInfoFragment;
 import com.android.settings.deviceinfo.firmwareversion.FirmwareVersionSettings;
 import com.android.settings.deviceinfo.legal.ModuleLicensesDashboard;
 import com.android.settings.display.AutoBrightnessSettings;
+import com.android.settings.display.DcDimmingSettings;
 import com.android.settings.display.NightDisplaySettings;
 import com.android.settings.display.ScreenTimeoutSettings;
 import com.android.settings.display.RefreshRateSettings;
@@ -346,6 +347,7 @@ public class SettingsGateway {
             ResetDashboardFragment.class.getName(),
             NightDisplaySettings.class.getName(),
             RefreshRateSettings.class.getName(),
+            DcDimmingSettings.class.getName(),
             ManageDomainUrls.class.getName(),
             AutomaticStorageManagerSettings.class.getName(),
             StorageDashboardFragment.class.getName(),
