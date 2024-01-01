SuperiorOS/android_packages_apps_Settings

Settings: Add device name inside settings firmware version layout
- Re-arrange

Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
Change-Id: Ib814014cabd60b40b2e997695a387d2b3630907b

src/com/android/settings/deviceinfo/firmwareversion/AboutDeviceNamePreferenceController.java
@@ -1,0 +1,45 @@
+/*
+ * Copyright (C) 2019 ArrowOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.deviceinfo.firmwareversion;
+
+import android.content.Context;
+import android.os.SystemProperties;
+
+import com.android.settings.R;
+import com.android.settings.core.BasePreferenceController;
+
+public class AboutDeviceNamePreferenceController extends BasePreferenceController {
+
+    private static final String TAG = "AboutDeviceNameCtrl";
+
+    private static final String KEY_DEVICE_NAME_PROP = "ro.superior.device";
+
+    public AboutDeviceNamePreferenceController(Context context, String key) {
+        super(context, key);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return AVAILABLE;
+    }
+
+    @Override
+    public CharSequence getSummary() {
+        return SystemProperties.get(KEY_DEVICE_NAME_PROP,
+                mContext.getString(R.string.unknown));
+    }
+}

osm1019/packages_apps_Settings_mist

Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

src/com/android/settings/deviceinfo/firmwareversion/AboutDeviceNamePreferenceController.java
@@ -1,0 +1,45 @@
+/*
+ * Copyright (C) 2019 ArrowOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.deviceinfo.firmwareversion;
+
+import android.content.Context;
+import android.os.SystemProperties;
+
+import com.android.settings.R;
+import com.android.settings.core.BasePreferenceController;
+
+public class AboutDeviceNamePreferenceController extends BasePreferenceController {
+
+    private static final String TAG = "AboutDeviceNameCtrl";
+
+    private static final String KEY_DEVICE_NAME_PROP = "ro.cherish.device";
+
+    public AboutDeviceNamePreferenceController(Context context, String key) {
+        super(context, key);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return AVAILABLE;
+    }
+
+    @Override
+    public CharSequence getSummary() {
+        return SystemProperties.get(KEY_DEVICE_NAME_PROP,
+                mContext.getString(R.string.unknown));
+    }
+}