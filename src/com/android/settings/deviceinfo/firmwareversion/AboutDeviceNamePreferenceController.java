172f75a6534742b66f86dc1aaac6f0f30d415a58
ShukakuZa <yusufzuzu787@gmail.com>
2024-01-07 07:13:11 +0000

    Settings: Import Some build libs

--- src/com/android/settings/deviceinfo/firmwareversion/AboutDeviceNamePreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/AboutDeviceNamePreferenceController.java
@@ -17,7 +17,15 @@
 package com.android.settings.deviceinfo.firmwareversion;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.SystemProperties;
+import android.text.TextUtils;
+import android.util.Log;
+
+import androidx.preference.Preference;
 
 import com.android.settings.R;
 import com.android.settings.core.BasePreferenceController;
