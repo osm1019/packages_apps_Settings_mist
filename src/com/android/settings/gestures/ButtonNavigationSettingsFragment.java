SuperiorOS/android_packages_apps_Settings

Settings: Add navbar layout inversion tuning [2/2]
Change-Id: I298cc60efdbab7508d6e903d61bc638f1e01360e
Signed-off-by: S I P U N <sipunkumar85@gmail.com>

src/com/android/settings/gestures/ButtonNavigationSettingsFragment.java
@@ -21,6 +21,9 @@ import static android.view.WindowManagerPolicyConstants.NAV_BAR_MODE_3BUTTON_OVE
 
 import android.app.settings.SettingsEnums;
 import android.content.Context;
+import android.os.Bundle;
+
+import androidx.preference.SwitchPreference;
 
 import com.android.settings.R;
 import com.android.settings.dashboard.DashboardFragment;
@@ -38,6 +41,22 @@ public class ButtonNavigationSettingsFragment extends DashboardFragment {
     public static final String BUTTON_NAVIGATION_SETTINGS =
             "com.android.settings.BUTTON_NAVIGATION_SETTINGS";
 
+    private static final String KEY_NAVBAR_INVERT = "sysui_nav_bar_inverse";
+
+    private SwitchPreference mNavbarInvertPref;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        SwitchPreference navbarInvertPref = (SwitchPreference) findPreference(KEY_NAVBAR_INVERT);
+        final Bundle arguments = getArguments();
+        if (arguments != null) {
+            final String key = arguments.getString(SystemNavigationGestureSettings.KEY_SYSTEM_NAV);
+            if (key != null && !key.equals(SystemNavigationGestureSettings.KEY_SYSTEM_NAV_3BUTTONS))
+                getPreferenceScreen().removePreference(navbarInvertPref);
+        }
+    }
 
     @Override
     public int getMetricsCategory() {

osm1019/packages_apps_Settings_mist

src/com/android/settings/gestures/ButtonNavigationSettingsFragment.java
@@ -21,6 +21,9 @@ import static android.view.WindowManagerPolicyConstants.NAV_BAR_MODE_3BUTTON_OVE
 
 import android.app.settings.SettingsEnums;
 import android.content.Context;
+import android.os.Bundle;
+
+import androidx.preference.SwitchPreferenceCompat;
 
 import com.android.settings.R;
 import com.android.settings.dashboard.DashboardFragment;
@@ -38,6 +41,22 @@ public class ButtonNavigationSettingsFragment extends DashboardFragment {
     public static final String BUTTON_NAVIGATION_SETTINGS =
             "com.android.settings.BUTTON_NAVIGATION_SETTINGS";
 
+    private static final String KEY_NAVBAR_INVERT = "sysui_nav_bar_inverse";
+
+    private SwitchPreferenceCompat mNavbarInvertPref;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        SwitchPreferenceCompat navbarInvertPref = (SwitchPreferenceCompat) findPreference(KEY_NAVBAR_INVERT);
+        final Bundle arguments = getArguments();
+        if (arguments != null) {
+            final String key = arguments.getString(SystemNavigationGestureSettings.KEY_SYSTEM_NAV);
+            if (key != null && !key.equals(SystemNavigationGestureSettings.KEY_SYSTEM_NAV_3BUTTONS))
+                getPreferenceScreen().removePreference(navbarInvertPref);
+        }
+    }
 
     @Override
     public int getMetricsCategory() {