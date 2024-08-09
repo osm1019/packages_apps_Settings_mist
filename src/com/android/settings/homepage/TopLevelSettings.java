ProjectEverest-staging/packages_apps_Settings

Settings: Use BaseCamp cards for homepage

src/com/android/settings/homepage/TopLevelSettings.java
@@ -269,7 +269,7 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
             	|| key.equals("top_level_apps")
             	|| key.equals("top_level_accessibility")
                 || key.equals("top_level_system")){
-                preference.setLayoutResource(R.layout.everestos_dashboard_preference_top);
+                preference.setLayoutResource(R.layout.everest_dashboard_preference_top);
             } else if (key.equals("top_level_battery")
               || key.equals("top_level_wallpaper")
             	|| key.equals("top_level_security")
@@ -278,17 +278,17 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
             	|| key.equals("top_level_wellbeing")
             	|| key.equals("top_level_location")
             	|| key.equals("top_level_notifications")){
-                preference.setLayoutResource(R.layout.everestos_dashboard_preference_middle);
+                preference.setLayoutResource(R.layout.everest_dashboard_preference_middle);
             } else if ("top_level_google".equals(key)){
-                preference.setLayoutResource(R.layout.everestos_dashboard_preference_bottom);
+                preference.setLayoutResource(R.layout.everest_dashboard_preference_bottom);
             } else if (key.equals("top_level_accounts") && gAppsExists){
-                preference.setLayoutResource(R.layout.everestos_dashboard_preference_middle);
+                preference.setLayoutResource(R.layout.everest_dashboard_preference_middle);
             } else if (key.equals("top_level_basecamp")){
-                preference.setLayoutResource(R.layout.everestos_dashboard_preference_single);
+                preference.setLayoutResource(R.layout.everest_dashboard_preference_single);
             } else if (key.equals("top_level_about_device")){
                 preference.setLayoutResource(R.layout.custom_dashboard_top);
             } else {
-                preference.setLayoutResource(R.layout.everestos_dashboard_preference_bottom);
+                preference.setLayoutResource(R.layout.everest_dashboard_preference_bottom);
             }
        }
     }

osm1019/packages_apps_Settings_mist

src/com/android/settings/homepage/TopLevelSettings.java
@@ -270,7 +270,7 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
             	|| key.equals("top_level_apps")
             	|| key.equals("top_level_accessibility")
                 || key.equals("top_level_system")){
-                preference.setLayoutResource(R.layout.mistos_dashboard_preference_top);
+                preference.setLayoutResource(R.layout.mist_dashboard_preference_top);
             } else if (key.equals("top_level_battery")
               || key.equals("top_level_wallpaper")
             	|| key.equals("top_level_security")
@@ -279,17 +279,17 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
             	|| key.equals("top_level_wellbeing")
             	|| key.equals("top_level_location")
             	|| key.equals("top_level_notifications")){
-                preference.setLayoutResource(R.layout.mistos_dashboard_preference_middle);
+                preference.setLayoutResource(R.layout.mist_dashboard_preference_middle);
             } else if ("top_level_google".equals(key)){
-                preference.setLayoutResource(R.layout.mistos_dashboard_preference_bottom);
+                preference.setLayoutResource(R.layout.mist_dashboard_preference_bottom);
             } else if (key.equals("top_level_accounts") && gAppsExists){
-                preference.setLayoutResource(R.layout.mistos_dashboard_preference_middle);
+                preference.setLayoutResource(R.layout.mist_dashboard_preference_middle);
             } else if (key.equals("top_level_mistify")){
-                preference.setLayoutResource(R.layout.mistos_dashboard_preference_single);
+                preference.setLayoutResource(R.layout.mist_dashboard_preference_single);
             } else if (key.equals("top_level_about_device")){
                 preference.setLayoutResource(R.layout.custom_dashboard_top);
             } else {
-                preference.setLayoutResource(R.layout.mistos_dashboard_preference_bottom);
+                preference.setLayoutResource(R.layout.mist_dashboard_preference_bottom);
             }
        }
     }