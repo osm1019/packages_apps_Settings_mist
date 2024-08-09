ProjectMatrixx/android_packages_apps_Settings

SoftAp: add client and AP band manager [SQUASH]
[jhonboy121]: adapted to A13

mnri: consolidated ap band changes and minor optimization changes, forward port to U QPR2

Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
Signed-off-by: cjybyjk <cjybyjk@zjnu.edu.cn>
Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -196,6 +196,8 @@ import com.android.settings.wifi.tether.WifiTetherSettings;
 import com.crdroid.settings.crDroidSettingsLayout;
 import com.crdroid.settings.fragments.ui.SmartPixels;
 
+import con.android.settings.wifi.tether.WifiTetherClientManager;
+
 public class SettingsGateway {
 
     /**
@@ -379,6 +381,7 @@ public class SettingsGateway {
             crDroidSettingsLayout.class.getName(),
             RunningServices.class.getName(),
             SmartPixels.class.getName(),
+            WifiTetherClientManager.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {

osm1019/packages_apps_Settings_mist

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -209,6 +209,8 @@ import com.mist.mist.Mistify;
 
 import com.android.settings.livedisplay.LiveDisplaySettings;
 
+import con.android.settings.wifi.tether.WifiTetherClientManager;
+
 public class SettingsGateway {
 
     /**
@@ -403,7 +405,8 @@ public class SettingsGateway {
             RunningServices.class.getName(),
             LiveDisplaySettings.class.getName(),
             VolumeSteps.class.getName(),
-            SleepMode.class.getName()
+            SleepMode.class.getName(),
+            WifiTetherClientManager.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {